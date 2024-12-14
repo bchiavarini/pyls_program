19f8597b93e28e96672a94ee6eb514885e38c18d
bchiavar <b.chiavarini@cineca.it>
2024-12-14 18:53:29 +0100

    included -t option for time sorting

--- pyls.py
+++ pyls.py
@@ -1,13 +1,15 @@
 import argparse
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Dict, List,Optional
+from typing import Any, Dict, List, Optional
 import json
 
 CONFIG_FILEPATH = "structure.json"
 
+
 class JsonDecoderException(Exception):
     """Exception raised when the config Json object cannot be decoded."""
+
 
 def convert_time(epoch_time: int) -> str:
     # convert epoch time to datetime relative to the user time zone
@@ -15,9 +17,21 @@ def convert_time(epoch_time: int) -> str:
     formatted_time = dt_object.strftime('%b %d %H:%M')
     return formatted_time
 
-def list_content(structure:Dict[str, Any],include_hidden:bool=False,long_listing:bool=False) -> List[str]:
-    content:List[str] = []
-    for el in structure["contents"]:
+
+def list_content(structure: Dict[str, Any],
+                 include_hidden: bool = False,
+                 long_listing: bool = False,
+                 time_sort:bool = False,) -> List[str]:
+    content: List[str] = []
+
+    if time_sort:
+        # sort the contents in structure by time
+        sorted_content=sorted(structure["contents"], key=lambda x: x["time_modified"])
+        content_to_parse = sorted_content
+    else:
+        content_to_parse = structure["contents"]
+
+    for el in content_to_parse:
         if el["name"].startswith(".") and not include_hidden:
             # ignore the hidden files
             continue
@@ -33,15 +47,18 @@ def list_content(structure:Dict[str, Any],include_hidden:bool=False,long_listing
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="ls",description=f"List the contents of directories described in {CONFIG_FILEPATH}")
+    parser = argparse.ArgumentParser(prog="ls",
+                                     description=f"List the contents of directories described in {CONFIG_FILEPATH}")
     parser.add_argument('-A', dest='include_hidden', action='store_true',
                         help='List all contents including hidden files and directories')
     parser.add_argument('-l', dest='long_listing', action='store_true',
                         help='Use a long listing format')
     parser.add_argument('-r', dest='reverse', action='store_true',
                         help='Reverse order while sorting')
+    parser.add_argument('-t', dest='time_sort', action='store_true',
+                        help='Sort by modification time (oldest first)')
     args = parser.parse_args()
-    config_file= Path(CONFIG_FILEPATH)
+    config_file = Path(CONFIG_FILEPATH)
     if not config_file.exists() or not config_file.is_file():
         raise FileNotFoundError("structure.json not found")
     # open the structure file
@@ -51,7 +68,10 @@ def main():
     except Exception:
         raise JsonDecoderException("structure.json cannot be read")
 
-    content = list_content(directories_structure,include_hidden=args.include_hidden,long_listing=args.long_listing)
+    content = list_content(directories_structure,
+                           include_hidden=args.include_hidden,
+                           long_listing=args.long_listing,
+                           time_sort=args.time_sort)
     if args.reverse:
         # reverse the content order
         content.reverse()
@@ -61,7 +81,6 @@ def main():
             print(el)
     else:
         print(" ".join(content))
-
 
 
 if __name__ == "__main__":
--- tests/test_pyls_functions.py
+++ tests/test_pyls_functions.py
@@ -15,7 +15,7 @@ MOCK_STRUCTURE = {
     {
       "name": "LICENSE",
       "size": 1071,
-      "time_modified": 1699941437,
+      "time_modified": 1699950073,
       "permissions": "drwxr-xr-x"
     },
     {
@@ -36,4 +36,9 @@ def test_include_hidden():
 
 def test_long_list():
     result = list_content(MOCK_STRUCTURE, long_listing=True)
-    assert result == ["drwxr-xr-x   1071 Nov 14 06:57 LICENSE", "drwxr-xr-x     83 Nov 14 06:57 README.md"]
+    assert result == ["drwxr-xr-x   1071 Nov 14 09:21 LICENSE", "drwxr-xr-x     83 Nov 14 06:57 README.md"]
+
+def test_time_sort():
+    # make sure of the order of the epoch time
+    result = list_content(MOCK_STRUCTURE, time_sort=True)
+    assert result == ["README.md","LICENSE"]
